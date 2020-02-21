f449e8919ef987543ca02859fd0e2f87a8e3237e
PixelFest <nmattheij@gmail.com>
2020-02-21 18:04:36 +0100

    Small changes

--- Sistem2/UserControls/BaseLayerProperties.xaml.cs
+++ Sistem2/UserControls/BaseLayerProperties.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using Sistem2.ViewModels;
 
 namespace Sistem2
@@ -9,6 +10,8 @@ namespace Sistem2
 	public partial class BaseLayerProperties : UserControl
 	{
 		private LayerBase _baseLayer => DataContext as LayerBase;
+		
+
 
 		/// <summary>
 		/// Constructor
